70989fb5de84e1d0544e5c93bc36e5d71a11262e
Wasim Patel <88201281+Cyberking0x1@users.noreply.github.com>
2022-01-09 20:37:02 +0530

    Update TCP_client.py

--- TCP_client.py
+++ TCP_client.py
@@ -20,3 +20,16 @@ client.send(data.encode('utf-8'))
 response = client.recv(4096)
 
 print(response)
+
+
+"""
+TCP Client:
+
+There have been countless times during penetration tests that I’ve needed to whip up a TCP client to
+test for services, send garbage data, fuzz, or any number of other tasks. If you are working within the
+confines of large enterprise environments, you won’t have the luxury of networking tools or
+compilers, and sometimes you’ll even be missing the absolute basics like the ability to copy/paste or
+an Internet connection. This is where being able to quickly create a TCP client comes in extremely
+handy.
+"""
+
